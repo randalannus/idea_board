bf7e3c7d0d9a036f4411dbcec905a78bd794d140
Randal Annus <randal.annus@gmail.com>
2022-07-05 12:53:12 +0300

    Optimize feed performance

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -6,37 +6,60 @@ import 'package:idea_board/ideas.dart';
 import 'package:provider/provider.dart';
 import 'package:tiktoklikescroller/tiktoklikescroller.dart';
 
-class FeedPage extends StatefulWidget {
+class FeedPage extends StatelessWidget {
   const FeedPage({Key? key}) : super(key: key);
 
   @override
-  State<FeedPage> createState() => _FeedPageState();
+  Widget build(BuildContext context) {
+    return Consumer<IdeasProvider>(
+      builder: (context, provider, _) => FutureBuilder<List<Idea>>(
+          future: provider.listIdeas(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const SizedBox();
+            }
+            final ideas = snapshot.data!;
+            return Scroller(ideas: ideas);
+          }),
+    );
+  }
 }
 
-class _FeedPageState extends State<FeedPage> {
+class Scroller extends StatefulWidget {
+  const Scroller({
+    Key? key,
+    required this.ideas,
+  }) : super(key: key);
+
+  final List<Idea> ideas;
+
+  @override
+  State<Scroller> createState() => _ScrollerState();
+}
+
+class _ScrollerState extends State<Scroller> {
   static const intMaxValue = 9007199254740991;
   final Map<int, int> indexToId = {};
+  final Random rng = Random();
 
   @override
   Widget build(BuildContext context) {
     return TikTokStyleFullPageScroller(
         contentSize: intMaxValue,
         builder: (context, index) {
-          return Consumer<IdeasProvider>(builder: (context, provider, _) {
-            final future = !indexToId.containsKey(index)
-                ? provider.selectRandom()
-                : provider.getIdea(indexToId[index]!);
-            return FutureBuilder<Idea>(
-                future: future,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const SizedBox();
-                  }
-                  final idea = snapshot.data!;
-                  indexToId[index] = idea.id;
-                  return IdeaCard(idea: idea);
-                });
-          });
+          Idea idea = indexToId.containsKey(index)
+              ? findIdea(indexToId[index]!)
+              : selectRandomIdea();
+          indexToId[index] = idea.id;
+          return IdeaCard(idea: idea);
         });
+  }
+
+  Idea findIdea(int id) {
+    return widget.ideas.firstWhere((idea) => idea.id == id);
+  }
+
+  Idea selectRandomIdea() {
+    return widget.ideas[rng.nextInt(widget.ideas.length)];
   }
 }
