32232fcb10c1a7c467cf990331740f1d6bbb9ab9
Randal Annus <randal.annus@gmail.com>
2022-08-15 16:32:02 +0300

    Add scrollbar to List Page

--- lib/ui/pages/list_page.dart
+++ lib/ui/pages/list_page.dart
@@ -22,20 +22,23 @@ class ListPage extends StatelessWidget {
 
   Widget ideasListView(List<Idea> unarchivedIdeas) {
     const double sepHeight = 8;
-    return ListView.separated(
-      key: const PageStorageKey("ideasList"),
-      itemCount:
-          unarchivedIdeas.length + 2, // Add separators to the beginning and end
-      itemBuilder: (context, index) {
-        if (index == 0 || index == unarchivedIdeas.length + 1) {
-          return const SizedBox();
-        }
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: IdeaCard(idea: unarchivedIdeas[index - 1]),
-        );
-      },
-      separatorBuilder: (context, index) => const SizedBox(height: sepHeight),
+    return Scrollbar(
+      radius: const Radius.circular(20),
+      child: ListView.separated(
+        key: const PageStorageKey("ideasList"),
+        itemCount: unarchivedIdeas.length +
+            2, // Add separators to the beginning and end
+        itemBuilder: (context, index) {
+          if (index == 0 || index == unarchivedIdeas.length + 1) {
+            return const SizedBox();
+          }
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: IdeaCard(idea: unarchivedIdeas[index - 1]),
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(height: sepHeight),
+      ),
     );
   }
 }
