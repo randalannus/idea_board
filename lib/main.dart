afa34712e1450bf5a635d91f7a16a689276fa707
Randal Annus <randal.annus@gmail.com>
2022-08-15 14:10:57 +0300

    Add "no connection" screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:animations/animations.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -49,13 +52,28 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Idea Board',
         theme: Themes.mainTheme,
-        home: Consumer<User?>(
-          builder: (context, user, _) {
-            return MyPageTransitionSwitcher(
-              transitionType: SharedAxisTransitionType.scaled,
-              child: user == null ? const SignInPage() : const HomePage(),
-            );
-          },
+        home: NetworkConnectivitySwitcher(
+          notConnectedChild: notConnectedChild(context),
+          connectedChild: Consumer<User?>(
+            builder: (context, user, _) {
+              return MyPageTransitionSwitcher(
+                transitionType: SharedAxisTransitionType.scaled,
+                child: user == null ? const SignInPage() : const HomePage(),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Scaffold notConnectedChild(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "No internet connection",
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headlineLarge,
         ),
       ),
     );
@@ -218,4 +236,81 @@ class MyPageTransitionSwitcher extends PageTransitionSwitcher {
           duration: duration,
           key: key,
         );
+}
+
+class NetworkConnectivitySwitcher extends StatefulWidget {
+  final Widget connectedChild;
+  final Widget notConnectedChild;
+
+  const NetworkConnectivitySwitcher({
+    required this.connectedChild,
+    required this.notConnectedChild,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<NetworkConnectivitySwitcher> createState() =>
+      _NetworkConnectivitySwitcherState();
+}
+
+class _NetworkConnectivitySwitcherState
+    extends State<NetworkConnectivitySwitcher> {
+  static const bufferDuration = Duration(seconds: 1);
+
+  final StreamController<bool> _isConnectedController = StreamController();
+  late final Stream<bool> _isConnectedStream =
+      _isConnectedController.stream.distinct();
+  Timer? _timer;
+
+  late final StreamSubscription<bool> _connectivitySubscription;
+
+  @override
+  void initState() {
+    Connectivity().checkConnectivity().then(
+          (result) => _connectionListener(result != ConnectivityResult.none),
+        );
+    _connectivitySubscription = Connectivity()
+        .onConnectivityChanged
+        .map<bool>((result) => result != ConnectivityResult.none)
+        .distinct()
+        .listen(_connectionListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _connectivitySubscription.cancel();
+    _isConnectedController.close();
+    super.dispose();
+  }
+
+  void _connectionListener(bool isConnected) {
+    _timer?.cancel();
+    if (isConnected) {
+      _isConnectedController.add(isConnected);
+    } else {
+      // "Not connected" events are delayed to avoid false alarms when switching
+      // from cellular data to wifi.
+      // If a "connected" event comes before the timer is finished then the
+      // timer is cancelled.
+      _timer =
+          Timer(bufferDuration, () => _isConnectedController.add(isConnected));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<bool>(
+      stream: _isConnectedStream,
+      initialData: true,
+      builder: (context, snapshot) {
+        bool isConnected = snapshot.data ?? true;
+        return MyPageTransitionSwitcher(
+          transitionType: SharedAxisTransitionType.scaled,
+          child: isConnected ? widget.connectedChild : widget.notConnectedChild,
+        );
+      },
+    );
+  }
 }
