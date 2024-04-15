422f96b72cbc4d74861063f56a86481560a7e333
Randal Annus <randal.annus@gmail.com>
2024-04-16 02:07:12 +0300

    add openai

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,18 @@
 const { onRequest } = require("firebase-functions/v2/https");
-const { defineSecret } = require('firebase-functions/params');
-
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-const googleAiApiKey = defineSecret("GOOGLE_AI_API_KEY");
+const { defineSecret } = require("firebase-functions/params");
+const { OpenAI } = require("openai");
 
 
-exports.test = onRequest(
-  { secrets: [googleAiApiKey] },
-  (req, res) => {
-    res.status(200).send(googleAiApiKey.value());
+const openAIApiKey = defineSecret("OPENAI_API_KEY");
+
+exports.generateanswer = onRequest(
+  { secrets: [openAIApiKey] },
+  async (req, res) => {
+    const openai = new OpenAI({ apiKey: openAIApiKey.value() });
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-4-turbo-2024-04-09",
+    });
+    res.status(200).send(completion.choices[0].message.content);
   }
 )
