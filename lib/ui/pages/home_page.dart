0c6b00eb1ccce20067fb2d06b951675d5acf75b8
Randal Annus <randal.annus@gmail.com>
2024-04-27 18:17:21 +0300

    Refactor recordingPage

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -10,7 +10,7 @@ import 'package:idea_board/service/recorder_service.dart';
 import 'package:idea_board/ui/pages/chat_page.dart';
 import 'package:idea_board/ui/pages/feed_page.dart';
 import 'package:idea_board/ui/pages/list_page.dart';
-import 'package:idea_board/ui/pages/record_page.dart';
+import 'package:idea_board/ui/pages/recording_page.dart';
 import 'package:idea_board/ui/pages/write_page.dart';
 import 'package:idea_board/ui/widgets/confimation_dialog.dart';
 import 'package:idea_board/ui/widgets/transition_switcher.dart';
@@ -155,7 +155,7 @@ class _HomePageState extends State<HomePage> {
             Provider.value(value: ideasService),
             Provider.value(value: user),
           ],
-          child: const RecordPage(),
+          child: const RecordingPage(),
         ),
       ),
     );
